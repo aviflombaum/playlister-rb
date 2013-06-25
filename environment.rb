fcc0434762549039d21499034e8615910e0a27eb
aviflombaum <avi@flombaum.com>
2013-06-25 11:03:26 -0400

    better environment

--- environment.rb
+++ environment.rb
@@ -1,11 +1,11 @@
-require_relative 'lib/concerns/memorable'
-require_relative 'lib/concerns/findable'
-require_relative 'lib/concerns/listable'
+# require_relative 'lib/concerns/memorable'
+# require_relative 'lib/concerns/findable'
+# require_relative 'lib/concerns/listable'
 
-require_relative 'lib/models/artist'
-require_relative 'lib/models/song'
-require_relative 'lib/models/genre'
-require_relative 'lib/models/library_parser'
+# require_relative 'lib/models/artist'
+# require_relative 'lib/models/song'
+# require_relative 'lib/models/genre'
+# require_relative 'lib/models/library_parser'
 
 Dir.glob('./lib/concerns/*.rb').each do |concern|
   require concern
